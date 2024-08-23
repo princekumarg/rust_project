c23967d271adbaa3461eb7554814721f4e2621d4
PRINCE KUMAR <86905668+princekumarg@users.noreply.github.com>
2024-08-23 22:58:22 +0530

    Delete variables/target directory

--- variables/target/debug/deps/variables-87cb5ec98b5133fc.d
+++
@@ -1,5 +0,0 @@
-/home/princekumar9508/rust_project/variables/target/debug/deps/variables-87cb5ec98b5133fc: src/main.rs
-
-/home/princekumar9508/rust_project/variables/target/debug/deps/variables-87cb5ec98b5133fc.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/home/princekumar9508/rust_project/variables/target/debug/variables: /home/princekumar9508/rust_project/variables/src/main.rs
