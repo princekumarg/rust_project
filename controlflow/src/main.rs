71e332f4f06d0765a3b8ef6badd4457377f338be
Prince Kumar <agarwal8789@gmail.com>
2024-08-30 22:13:28 +0530

    commit made on control flow

---
+++ controlflow/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let number = 6;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+    println!("is even{}",is_even(5));
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+fn is_even(n:i32)->bool{
+    if n%2==0{
+        return true;
+    }
+    println!("not an even no");
+    return false;
+}
